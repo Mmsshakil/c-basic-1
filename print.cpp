2ec73f1e6e677eb0eb0885dd8b8eb4e6ee18ea9e
Mmsshakil <softhandshakil@gmail.com>
2025-05-25 03:45:31 +0600

    basic print on c++;

---
+++ print.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main()
+{
+    int x = 45;
+    char c = 'X';
+    float f = 25.482;
+
+    std::cout << x << " " << c << std::endl;
+    std::cout << "The value of x " << x << std::endl;
+
+    return 0;
+}
