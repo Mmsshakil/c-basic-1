a372a8a9e3bef222bb7a140eed6b89143202c448
Mmsshakil <softhandshakil@gmail.com>
2025-05-26 02:19:43 +0600

    string

---
+++ string.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    // int x;
+    // char s[100];
+    string s;
+    cin >> s;
+
+    // cin.ignore(); // when we will face using getline or fgets
+    // cin >> x;
+
+    // cin >> s;           // if there is not any space in string
+    // fgets(s, 100, stdin);  // as like c language
+    // cin.getline(s, 100); // we always use this when with space
+
+    // cout << x << endl;
+    cout << s << endl;
+    return 0;
+}
