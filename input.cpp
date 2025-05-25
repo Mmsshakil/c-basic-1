2d9797836077f1b1e43b3aba11e19dff5e672afc
Mmsshakil <softhandshakil@gmail.com>
2025-05-25 22:43:55 +0600

    ascii value of char

--- input.cpp
+++ input.cpp
@@ -4,8 +4,14 @@ using namespace std;
 int main()
 {
     int x, y;
+    char c = 'A';
+
     cin >> x >> y;
-    cout << x << " " << y;
+    cout << x << " " << y << " " << c << endl;
+
+    // print the ascii value of char c
+    int ascii = c;
+    cout << ascii << endl;
 
     return 0;
 }
